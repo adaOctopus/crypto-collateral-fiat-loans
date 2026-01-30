5bd6e07551c33e65d9f76bfa25d6c7c542f3b17a
gandalfg <tasos.valtinos@gmail.com>
2026-01-30 17:24:51 +0100

    vm deal 1 eth, due to min buy requirement, test passes

--- contracts/test/LoanSecuritization.t.sol
+++ contracts/test/LoanSecuritization.t.sol
@@ -14,6 +14,9 @@ contract LoanSecuritizationTest is Test {
     function setUp() public {
         owner = address(this);
         user = address(0x1);
+        // Mixing contract interaction testing with VerificationNFT
+
+
         verificationNFT = new VerificationNFT(owner);
         loanSecuritization = new LoanSecuritization(address(verificationNFT), owner);
         // Allow this test contract to mint (normally CollateralLock is minter)
@@ -24,7 +27,34 @@ contract LoanSecuritizationTest is Test {
         assertEq(address(loanSecuritization.verificationNFT()), address(verificationNFT));
     }
 
+    
+
     function test_Securitize() public {
+        // Mint Verification NFT token 0 to user (simulates lock-collateral flow)
+        verificationNFT.mintVerificationNFT(user, "https://test.com/0");
+        // User must own the Verification NFT to securitize
+        vm.prank(user);
+        uint256 loanId = loanSecuritization.securitize(0);
+        assertEq(loanId, 0);
+    }
+
+    function test_BuyFraction() public {
+        // Mint Verification NFT token 0 to user (simulates lock-collateral flow)
+        verificationNFT.mintVerificationNFT(user, "https://test.com/0");
+        // User must own the Verification NFT to securitize
+        vm.prank(user);
+        uint256 loanId = loanSecuritization.securitize(0);
+        assertEq(loanId, 0);
+
+        // Buyer must have ETH to pay PRICE_PER_FRACTION
+        vm.deal(user, 1 ether);
+
+        // Buy a fraction
+        vm.prank(user);
+        loanSecuritization.buyFraction{value: 0.0000001 ether}(loanId);
+    }
+
+    function test_Withdraw() public {
         // Mint Verification NFT token 0 to user (simulates lock-collateral flow)
         verificationNFT.mintVerificationNFT(user, "https://test.com/0");
         // User must own the Verification NFT to securitize
